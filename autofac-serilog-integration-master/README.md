c84d087fdf7e1ca64c7ac0a4da1eb2783200cf50
Raghu Rudra <raghu.rudra@blackboard.com>
2016-12-01 22:39:51 -0700

    Initial

---
+++ autofac-serilog-integration-master/README.md
@@ -0,0 +1,57 @@
+# Contextual logger injection for Autofac
+
+[![Build status](https://ci.appveyor.com/api/projects/status/lannw2ooxpnwgdp4/branch/master?svg=true)](https://ci.appveyor.com/project/NicholasBlumhardt/autofac-serilog-integration/branch/master)
+
+When using [Serilog](http://serilog.net), _contextual_ loggers attach the logging type's name to log events so they can later be found and filtered:
+
+```csharp
+var log = Log.ForContext<SomeClass>();
+log.Information("This event is tagged with 'SomeClass'");
+```
+
+Applications that use IoC often accept dependencies as constructor parameters:
+
+```csharp
+public class SomeClass
+{
+  readonly ILogger _log;
+  
+  public SomeClass(ILogger log)
+  {
+    _log = log;
+  }
+  
+  public void Show()
+  {
+    _log.Information("This is also an event from 'SomeClass'");
+  }
+}
+```
+
+This library configures [Autofac](http://autofac.org) to automatically configure the correct contextual logger for each class into which an `ILogger` is injected.
+
+### Usage
+
+First install from NuGet:
+
+```powershell
+Install-Package AutofacSerilogIntegration
+```
+
+Next, create the _root logger_:
+
+```csharp
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.ColoredConsole()
+    .CreateLogger();
+```
+
+Then when configuring the Autofac container, call `RegisterLogger()`:
+
+```csharp
+var builder = new ContainerBuilder();
+
+builder.RegisterLogger();
+```
+
+If no logger is explicitly passed to this function, the default `Log.Logger` will be used.
---
+++ autofac-serilog-integration-master/src/AutofacSerilogIntegration/ContextualLoggingModule.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Activators.Reflection;
+using Serilog;
+using Module = Autofac.Module;
+
+namespace AutofacSerilogIntegration
+{
+    internal class ContextualLoggingModule : Module
+    {
+        const string TargetTypeParameterName = "Autofac.AutowiringPropertyInjector.InstanceType";
+
+        readonly ILogger _logger;
+        readonly bool _autowireProperties;
+        readonly bool _skipRegistration;
+
+        [Obsolete("Do not use this constructor. This is required by the Autofac assembly scanning")]
+        public ContextualLoggingModule()
+        {
+            // Workaround to skip the logger registration when module is loaded by Autofac assembly scanning
+            _skipRegistration = true;
+        }
+
+        internal ContextualLoggingModule(ILogger logger = null, bool autowireProperties = false)
+        {
+            _logger = logger;
+            _autowireProperties = autowireProperties;
+            _skipRegistration = false;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (_skipRegistration)
+                return;
+
+            builder.Register((c, p) =>
+            {
+                var logger = _logger ?? Log.Logger;
+
+                var targetType = p.OfType<NamedParameter>()
+                    .FirstOrDefault(np => np.Name == TargetTypeParameterName && np.Value is Type);
+
+                if (targetType != null)
+                    return logger.ForContext((Type) targetType.Value);
+
+                return logger;
+            })
+            .As<ILogger>()
+            .ExternallyOwned();
+        }
+
+        protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
+            IComponentRegistration registration)
+        {
+            if (_skipRegistration)
+                return;
+
+            // Ignore components that provide loggers (and thus avoid a circular dependency below)
+            if (registration.Services.OfType<TypedService>().Any(ts => ts.ServiceType == typeof (ILogger)))
+                return;
+
+            PropertyInfo[] targetProperties = null;
+
+            var ra = registration.Activator as ReflectionActivator;
+            if (ra != null)
+            {
+                var ctors = ra.ConstructorFinder.FindConstructors(ra.LimitType);
+                var usesLogger =
+                    ctors.SelectMany(ctor => ctor.GetParameters()).Any(pi => pi.ParameterType == typeof (ILogger));
+
+                if (_autowireProperties)
+                {
+                    var logProperties = ra.LimitType
+                                            .GetRuntimeProperties()
+                                            .Where(c => c.CanWrite && c.PropertyType == typeof(ILogger) && c.SetMethod.IsPublic && !c.SetMethod.IsStatic)
+                                            .ToArray();
+
+                    if (logProperties.Any())
+                    {
+                        targetProperties = logProperties;
+                        usesLogger = true;
+                    }
+                }
+
+                // Ignore components known to be without logger dependencies
+                if (!usesLogger)
+                    return;
+            }
+
+            registration.Preparing += (sender, args) =>
+            {
+                var log = args.Context.Resolve<ILogger>().ForContext(registration.Activator.LimitType);
+                args.Parameters = new[] {TypedParameter.From(log)}.Concat(args.Parameters);
+            };
+
+            if (targetProperties != null)
+            {
+                registration.Activating += (sender, args) =>
+                {
+                    var log = args.Context.Resolve<ILogger>().ForContext(registration.Activator.LimitType);
+                    foreach (var targetProperty in targetProperties)
+                    {
+                        targetProperty.SetValue(args.Instance, log);
+                    }
+                };
+            }
+        }
+    }
+}
